a5f90b7d01d3684467db96510a4a94fae255c78d
Ruan Victor <ruan.victor@aluno.uepb.edu.br>
2025-05-23 17:24:03 -0300

    GET LISTS

---
+++ src/main/java/com/devsuperior/dslist/controler/GameListController.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.controler;
+
+import com.devsuperior.dslist.dto.GameListDto;
+import com.devsuperior.dslist.services.GameListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping(value="/lists")
+public class GameListController {
+    @Autowired
+   private GameListService gameListService;
+
+    @GetMapping
+    public List<GameListDto> findAll(){
+        List<GameListDto> result = gameListService.findAll();
+        return result;
+    }
+
+}
