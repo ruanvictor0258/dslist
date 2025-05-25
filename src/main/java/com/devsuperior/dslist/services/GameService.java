d83847683d1864ddd91e609ff6d5ffc63334f39b
Ruan Victor <ruan.victor@aluno.uepb.edu.br>
2025-05-25 14:16:47 -0300

    GET LISTS

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDto;
 import com.devsuperior.dslist.dto.GameMinDto;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +30,11 @@ public class GameService {
        List<Game> result = gameRepository.findAll();
        List<GameMinDto> dto=result.stream().map(x -> new GameMinDto(x)).toList();
        return dto;
+    }
+    @Transactional(readOnly = true)
+    public List<GameMinDto> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDto(x)).toList();
+
     }
 }
